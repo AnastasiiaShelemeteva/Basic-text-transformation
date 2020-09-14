6b7c5e5b58e5957342dfc525be8b3a7b5ae040bc
Shelemeteva <A.Shelemeteva@home24.de>
2020-09-14 15:56:00 +0200

    code to make letters in lower case

---
+++ lower.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+	"fmt"
+	"strings"
+)
+
+func lowerCase(input string) {
+	output := strings.ToLower(input)
+	fmt.Println(output)	
+	}
+
+func main() {
+lowerCase("FHEUDDH")
+}
