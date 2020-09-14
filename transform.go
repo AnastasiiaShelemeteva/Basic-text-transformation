e5133b887646e9d146769d65de66fd6a8ee63f0b
Shelemeteva <A.Shelemeteva@home24.de>
2020-09-14 15:59:05 +0200

    added main file

---
+++ transform.go
@@ -0,0 +1,28 @@
+package main 
+
+import (
+	"fmt"
+	"strings"
+)
+/* /var import []string 
+fmt.Scanln(&import)
+
+func reverse(input string) {
+	for i := len(input) - 1; i >= 0; i-- {
+		fmt.Println(input[i])
+	} 
+} 
+
+func main() {
+	reverse("hey")
+}  */
+
+/* func main () {
+	var a[] string = [] string {"hallo", "fdsdf", "hgfolpj"}
+	for i := len(a) - 1; i >= 0; i-- {
+		fmt.Println(a[i])
+	}
+} // here I am trying to find out, how to get my input reverse. It works with numbers and words (each one of them a string, but how to take one string and make the letters #
+	inside to be reversed */
+
+
