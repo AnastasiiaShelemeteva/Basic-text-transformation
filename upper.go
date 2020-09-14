a5a5e89c21db503ca837f788c46121fcedc18b62
Shelemeteva <A.Shelemeteva@home24.de>
2020-09-14 15:50:06 +0200

    code to make an input in lowercase

---
+++ upper.go
@@ -0,0 +1,15 @@
+package main 
+
+import (
+	"fmt"
+	"strings"
+	)
+
+func upperCase(input string) {
+	output := strings.ToUpper(input)
+	fmt.Println(output)	
+	}
+
+func main() {
+upperCase("dsfsdfsd")
+}
